0858e312d6ffb828fe0027f40d6940e1e751df02
Badaranarayanareddy alavala <badaranarayanareddy.alavala@oracle.com>
2019-11-21 11:42:57 +0530

    added user repo

---
+++ src/repositories/repository.py
@@ -0,0 +1,57 @@
+"""
+
+This is the place to define database adapters
+
+
+"""
+
+class PostGresDBRepo(object):
+    
+    def connect():
+        pass
+
+    def execute():
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+
+
+class OracleDbRepo(object):
+    def connect():
+        pass
+
+    def execute():
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+
+
+class MangoDbRepo(object):
+    def connect():
+        pass
+
+    def execute():
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+
+
+class ElasticDbRepo(object):
+    pass
+
+
+
+
+
---
+++ src/repositories/user_repo.py
@@ -0,0 +1,31 @@
+"""
+Define repos to abstract orm models and queries
+
+"""
+from src.models.user_model import User  
+
+class UserRepo(object):
+     """
+     This class is write all user specific queries
+     """
+    def get(id):
+        # use the ORM models to query the database
+        return User.objects.filter(id=id)
+
+    def create(user_name, password, **kwargs):
+        user = User(user_name=user_name, password=password)
+        user.save()
+
+    def getAll():
+        return User.objects.all()
+
+    def get_active_users():
+        # write a query to get only active users
+        pass
+
+    def get_password_expired_users():
+        pass
+
+    def is_user_name_exits():
+        # write a query to check the user name existence in db
+        pass
